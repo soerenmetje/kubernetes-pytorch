d16a4cadfefba9e9a7dcd5148dbd3c44fdd112ca
Sören Metje <soerenmetje@yahoo.de>
2023-10-05 22:24:03 +0200

    Add selecting available device

--- src/main.py
+++ src/main.py
@@ -34,7 +34,14 @@ for X, y in test_dataloader:
     break
 
 # Get cpu, gpu or mps device for training.
-device = "cpu"
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
 print(f"Using {device} device")
 
 
